20af79713ce870cbb9b6de9e69adb5158c8e74e2
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-11-27 16:35:32 -0800

    adding profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home/Home'
-// import Parks from './pages/Home/Parks'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Parks from "./pages/Parks";
+import Profile from "./pages/Profile";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          {/* <Route exact path='/'>
-            <Home />
-          </Route> */}
-          <Route exact path="/" component={Home} />
-          {/* <Route exact path="/" component={Parks} /> */}
-
-          <Route>
-            <h1>404 NOT FOUND</h1>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route exact path="/Home"  component={Home} />
+					<Route exact path="/Parks" component={Parks} />
+          <Route exact path="/Profile" component={Profile} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
-export default App
+export default App;
