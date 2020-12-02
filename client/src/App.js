9c9b4028af6798f296eb1de52f6a8c776b975808
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-12-01 20:14:13 -0800

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Parks from "./pages/Parks";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./pages/Home/Home.css";
 function App() {
@@ -10,9 +11,10 @@ function App() {
 		<Router>
 			<div>
 				<Switch>
+					<Route exact path="/Login"  component={Login} />
 					<Route exact path="/Home"  component={Home} />
 					<Route exact path="/Parks" component={Parks} />
-          <Route exact path="/Profile" component={Profile} />
+        			<Route exact path="/Profile" component={Profile} />
 				</Switch>
 			</div>
 		</Router>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./Home.css";
-
 import { Link } from "react-router-dom";
 
 function Home() {
