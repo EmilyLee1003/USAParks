aa8c59fe2c57868a647bc54d2c9c876e653afe9c
Emily Lee <emlee088@gmail.com>
2020-12-08 14:50:10 -0800

    added information page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Parks from "./pages/Parks";
@@ -7,17 +7,35 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./pages/Home/Home.css";
+import Search from "./pages/search";
+import Information from "./pages/information";
+import Footer from "./components/footer";
+import Wrapper from "./components/wrapper";
+
+
+
 function App() {
+
+	const[value,setValue] = useState("");
+
+
 	return (
 		<Router>
 			<div>
-				<Switch>
+			<Wrapper>
 					<Route exact path="/Login"  component={Login} />
 					<Route exact path="/Signup"  component={Signup} />
 					<Route exact path="/Home"  component={Home} />
 					<Route exact path="/Parks" component={Parks} />
         			<Route exact path="/Profile" component={Profile} />
-				</Switch>
+
+					<Route path='/search'component={() => <Search setSearch={setValue}/>}
+        
+/>
+          <Route path='/information'component={() => <Information search={value}/>}
+/>
+			</Wrapper>
+			<Footer />
 			</div>
 		</Router>
 	);
