0327a3c47a34ee264eb98bfbc9ae08a3f07441d2
chilimango <maldonadot55@yahoo.com>
2020-12-08 22:12:17 -0800

    changed css classes to className

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,21 +15,21 @@ function LogIn() {
 	};
 	return (
 		<div>
-			<header class="lasthead">
-			<div class="container d-flex h-100 align-items-center">
-			<div class="mx-auto text-center">
-				<h1 class="mx-auto my-0 text-uppercase">Project Parks</h1>
-				{/* <!-- <h2 class="text-white-50 mx-auto mt-2 mb-5">
+			<header className="lasthead">
+			<div className="container d-flex h-100 align-items-center">
+			<div className="mx-auto text-center">
+				<h1 className="mx-auto my-0 text-uppercase">Project Parks</h1>
+				{/* <!-- <h2 className="text-white-50 mx-auto mt-2 mb-5">
 					See the beauty that lies in their own backyard
                 </h2> --> */}
 				<br></br>
 				<br></br>
 				<br></br>
-				<form class="form-inline d-flex">
+				<form className="form-inline d-flex">
 					<input
 						onChange = {(event)=>handleInputChange(event.target.name, event.target.value)}
 						name = 'email'
-						class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+						className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
 						id="email-input"
 						type="email"
                         placeholder="Email"
@@ -39,7 +39,7 @@ function LogIn() {
 					<input
 						// onChange = {(event)=>handleInputChange(event.target.name, event.target.value)}
 						name = 'password'
-						class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+						className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
 						id="inputEmail"
 						type="password"
                         placeholder="Password"
@@ -47,10 +47,10 @@ function LogIn() {
 					/>
 				</form>
 				<br></br>
-				<button onClick={handleLogInSubmit}  class="btn btn-primary js-scroll-trigger" href="#about">Log In</button>
+				<button onClick={handleLogInSubmit}  className="btn btn-primary js-scroll-trigger" href="#about">Log In</button>
 				<br></br>
 				<br></br>
-				{/* <button class="btn btn-primary js-scroll-trigger" href="#about">Sign Up</button> */}
+				{/* <button className="btn btn-primary js-scroll-trigger" href="#about">Sign Up</button> */}
 				<p id="changer">Or sign up <a href="./Signup">here</a></p>
 			</div>
 			</div>
