9a7175681c095680e90e7fc742675d6d2c33a5b7
chilimango <maldonadot55@yahoo.com>
2020-12-08 22:09:58 -0800

    added login server for user validation

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,22 @@
-const User = require( '../models/User.js')
-const express = require('express')
+const User = require( '../models/User.js');
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/users', (req, res) => {
-  User.find({}).then((users) => res.json(users))
-  db.collection('users').findOne(email, function(err, user){
+  User.findOne({}).then((users) => res.json(users))
     if(err) throw new Error(err);
     if(!user) 
       console.log('Not found');
     else 
       console.log('Found!');
-  })
-
-})
+});
 
 router.post('/createuser', ({ body }, res) => {
   console.log(body)
   User.create(body)
     .then((done) => res.json(done))
     .catch((err) => res.status(400).json(err))
-})
+});
 
 module.exports = router
